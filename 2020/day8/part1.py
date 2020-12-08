3d8173202b8ac1b5eb675a532d0dc693c71f24a2
Mohamed Haddi <m.haddi.95@gmail.com>
2020-12-08 13:29:16 +0100

    add my solution to the first part of 2020/day8

---
+++ 2020/day8/part1.py
@@ -0,0 +1,26 @@
+with open("input", "r") as file:
+    instructions = file.read().splitlines()
+
+been_here = set()
+acc, index = 0, 0
+length = len(instructions)
+
+while index < length:
+
+    if index in been_here:
+        break
+
+    been_here.add(index)
+    operation, argument = instructions[index].split(" ")
+
+    if operation == "nop":
+        index += 1
+
+    elif operation == "acc":
+        acc += int(argument)
+        index += 1
+
+    elif operation == "jmp":
+        index += int(argument)
+
+print(acc)
