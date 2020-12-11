78cbbdb0da5cf90adcd4178c985f345408338e58
Mohamed Haddi <m.haddi.95@gmail.com>
2020-12-12 00:47:07 +0100

    add comments

--- 2020/day11/part1.py
+++ 2020/day11/part1.py
@@ -11,34 +11,42 @@ def seat_checker(tmp, result, seat):
     for i in indices:
         adjacents = ""
 
+        # top left seat
         top_left = i - h_length - 1
         if top_left >= 0 and i % h_length != 0:
             adjacents += tmp[top_left]
 
+        # top seat
         top = i - h_length
         if top >= 0 and i >= h_length:
             adjacents += tmp[top]
 
+        # top right seat
         top_right = i - h_length + 1
         if top_right >= 0 and (i + 1) % h_length != 0:
             adjacents += tmp[top_right]
 
+        # left seat
         left = i - 1
         if left >= 0 and i % h_length != 0:
             adjacents += tmp[left]
 
+        # right seat
         right = i + 1
         if right < full_length and (i + 1) % h_length != 0:
             adjacents += tmp[right]
 
+        # bottom left seat
         bottom_left = i + h_length - 1
         if bottom_left < full_length and i % h_length != 0:
             adjacents += tmp[bottom_left]
 
+        # bottom seat
         bottom = i + h_length
         if bottom < full_length and i < full_length - h_length:
             adjacents += tmp[bottom]
 
+        # bottom right seat
         bottom_right = i + h_length + 1
         if bottom_right < full_length and (i + 1) % h_length != 0:
             adjacents += tmp[bottom_right]
