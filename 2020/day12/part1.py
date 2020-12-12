7bedb3b3071890c7817090971e459aa7230fb466
Mohamed Haddi <m.haddi.95@gmail.com>
2020-12-12 16:58:47 +0100

    clean part 1 code a bit

--- 2020/day12/part1.py
+++ 2020/day12/part1.py
@@ -2,39 +2,31 @@ with open("input", "r") as file:
     input = file.read().splitlines()
 
 directions = ["east", "south", "west", "north"]
-longitude, latitude, turn = 0, 0, 0
 current_direction = directions[0]
+longitude, latitude, turn = 0, 0, 0
 
 for instruction in input:
 
     action, value = instruction[0], int(instruction[1:])
 
-    if action == "F":
-        if current_direction == "east":
-            longitude += value
-        elif current_direction == "west":
-            longitude -= value
-        elif current_direction == "north":
-            latitude += value
-        elif current_direction == "south":
-            latitude -= value
-    elif action == "E":
-        longitude += value
-    elif action == "W":
-        longitude -= value
-    elif action == "N":
-        latitude += value
-    elif action == "S":
-        latitude -= value
-    elif action == "R":
-        turn = directions.index(current_direction) + value // 90
-        if turn > 3:
-            turn = turn - 4
-        current_direction = directions[turn]
+    if action == "R":
+        turn = directions.index(current_direction) + (value // 90)
+        current_direction = directions[turn if turn < 4 else turn - 4]
+
     elif action == "L":
-        turn = directions.index(current_direction) - value // 90
-        if turn < 0:
-            turn = turn + 4
-        current_direction = directions[turn]
+        turn = directions.index(current_direction) - (value // 90)
+        current_direction = directions[turn if turn >= 0 else turn + 4]
+
+    elif action == "E" or (action == "F" and current_direction == "east"):
+        longitude += value
+
+    elif action == "W" or (action == "F" and current_direction == "west"):
+        longitude -= value
+
+    elif action == "N" or (action == "F" and current_direction == "north"):
+        latitude += value
+
+    elif action == "S" or (action == "F" and current_direction == "south"):
+        latitude -= value
 
 print(abs(longitude) + abs(latitude))
