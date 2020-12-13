e9f5fab736f43240e693ae8b201f7575f4a46d42
Mohamed Haddi <m.haddi.95@gmail.com>
2020-12-13 15:00:26 +0100

    add my solution to part 1 of day 13

---
+++ 2020/day13/part1.py
@@ -0,0 +1,19 @@
+with open("input", "r") as file:
+    input = file.read().splitlines()
+
+timestamp = int(input[0])
+bus_ids = [int(bus_id) for bus_id in input[1].split(",") if bus_id.isdigit()]
+
+
+def check_bus_times(bus_id):
+    i, time = 0, bus_id
+    while time < timestamp:
+        time = bus_id * i
+        i += 1
+    return (time, bus_id)
+
+
+earliest_buses = list(map(lambda bus_id: check_bus_times(bus_id), bus_ids))
+earliest_bus_time, earliest_bus = min(earliest_buses)
+
+print(earliest_bus * (earliest_bus_time - timestamp))
