5c29a4977790a7ebc4b78920bffac4f1c52e7747
Mohamed Haddi <mhaddi@student.1337.ma>
2020-12-18 20:38:24 +0100

    day14/part1 solution

---
+++ 2020/day14/part1.py
@@ -0,0 +1,24 @@
+with open("input", "r") as file:
+    input = file.read().splitlines()
+
+mem = {}
+curr_mask, curr_value = 0, 0
+
+for line in input:
+
+    words = line.split()
+    words.remove("=")
+
+    if words[0] == "mask":
+        curr_mask = words[1]
+    else:
+        curr_value = format(int(words[1]), "036b")
+        new_value = "".join(
+            [
+                bit if char.isalpha() else char
+                for char, bit in zip(curr_mask, curr_value)
+            ]
+        )
+        exec("%s = %d" % (words[0], int(new_value, 2)))
+
+print(sum(mem.values()))
