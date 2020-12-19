9652d0e8007d740d25c8586fbebf2480d7342f6c
Mohamed Haddi <mhaddi@student.1337.ma>
2020-12-19 22:49:50 +0100

    finish solution to day16/part1

--- 2020/day16/part1.py
+++ 2020/day16/part1.py
@@ -1,18 +1,32 @@
-import pprint
-import functools
-
-with open("test-input", "r") as file:
+with open("input", "r") as file:
     input = list(map(lambda x: x.splitlines(), file.read().split("\n\n")))
 
 
 def split_ranges(x):
-    return list(map(lambda x: list(map(int, x.split("-"))), x.split(" ")[1::2]))
+    return list(
+        map(
+            lambda x: list(map(int, x.split("-"))),
+            x.split(":")[1].split(" ")[1::2],
+        )
+    )
 
 
 def split_tickets(x):
     return list(map(int, x.split(",")))
 
 
+def is_in_ranges(x):
+    valid = False
+    for field in input["fields"]:
+        for range in field:
+            min, max = range[0], range[1]
+            if min <= x <= max:
+                valid = True
+                break
+    return valid
+
+
+error_rate = 0
 input = {
     "fields": list(map(split_ranges, input[0])),
     "my-ticket": split_tickets(input[1][1]),
@@ -20,18 +34,10 @@ input = {
 }
 
 
-def val_not_in_ranges(x):
-    for field in input["fields"]:
-        for range in field:
-            min, max = range[0], range[1]
-            if not min <= x < -max:
-                return x
-    return 0
-
-
-error_rate = 0
 for ticket in input["nearby-tickets"]:
-    error_rate += functools.reduce(lambda acc, x: acc + val_not_in_ranges(x), ticket)
+    for val in ticket:
+        if not is_in_ranges(val):
+            error_rate += val
+            break
 
 print(error_rate)
-# pprint.pprint(input)
